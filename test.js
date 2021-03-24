210a4fb315105e3c6f0505fbbcd53aa1bbb5545c
king <1406788677@qq.com>
2021-03-24 17:10:49 +0800

    html加了个好玩的九宫格样式

--- test.js
+++ test.js
@@ -103,50 +103,88 @@
 //   console.log(time);
   
 
-let arr1=[1,2,3]
-let arr2 = [1,2,4,6,6,7]
+// let arr1=[1,2,3]
+// let arr2 = [1,2,4,6,6,7]
 
-let flag =  arr2.filter(item =>{
-  if(arr1.indexOf(item) == '-1'){
-    return true;
-  }else{
-    return false
-  }
-})
+// let flag =  arr2.filter(item =>{
+//   if(arr1.indexOf(item) == '-1'){
+//     return true;
+//   }else{
+//     return false
+//   }
+// })
 
-console.log(flag)
+// console.log(flag)
 
-if(this.isEdit){
-  let arr = this.$refs.uUploadMultiple.lists;
-  let arr2 = this.fileList;
-  let arr3=[];
-  let arr4 =[];
-  arr.forEach(item =>{
-    arr3.push(item.url)
-  })
-  arr2.forEach(item =>{
-    arr4.push(item.url)
-  })
-  let arr1 = this.filterData(arr4,arr3)
+// if(this.isEdit){
+//   let arr = this.$refs.uUploadMultiple.lists;
+//   let arr2 = this.fileList;
+//   let arr3=[];
+//   let arr4 =[];
+//   arr.forEach(item =>{
+//     arr3.push(item.url)
+//   })
+//   arr2.forEach(item =>{
+//     arr4.push(item.url)
+//   })
+//   let arr1 = this.filterData(arr4,arr3)
   
-  // console.log('list',this.$refs.uUploadMultiple.lists)
-  // console.log('listop',this.fileList)
-  // this.multipleUpload(this.$refs.uUploadMultiple.lists, 2)
-  //this.educationForm.projectLevel = this.projectLevel
-  console.log('arr1',arr1)
-  if(arr1.length>0){
-    this.uploadImgs(0, arr1.length, 0, arr1)
+//   // console.log('list',this.$refs.uUploadMultiple.lists)
+//   // console.log('listop',this.fileList)
+//   // this.multipleUpload(this.$refs.uUploadMultiple.lists, 2)
+//   //this.educationForm.projectLevel = this.projectLevel
+//   console.log('arr1',arr1)
+//   if(arr1.length>0){
+//     this.uploadImgs(0, arr1.length, 0, arr1)
+//   }
+// }
+
+// 	//过滤已经上传了的图片
+//   filterData(arr1,arr2){//arr1为新的列表，arr2为本身已经上传了的列表
+//     let newArr =  arr2.filter(item =>{
+//       if(arr1.indexOf(item) == '-1'){
+//         return true;
+//       }else{
+//         return false
+//       }
+//     })
+//     return newArr
+//   },let 
+
+function clone(obj){
+  if(obj instanceof Array){
+    return arrClone(obj)
+  }else if(obj instanceof Object){
+    return objClone(obj)
+  }else{
+    return obj
   }
 }
 
-	//过滤已经上传了的图片
-  filterData(arr1,arr2){//arr1为新的列表，arr2为本身已经上传了的列表
-    let newArr =  arr2.filter(item =>{
-      if(arr1.indexOf(item) == '-1'){
-        return true;
-      }else{
-        return false
-      }
-    })
-    return newArr
-  },
+function arrClone(obj){
+  let result = [];
+  for (let i = 0; i < obj.length; i++) {
+    result[i] = clone(obj[i]);
+    
+  }
+  return result;
+}
+
+function objClone(obj){
+  let result = {};
+  for(let prop in obj){
+    result[prop] = clone(obj[prop])
+  }               
+  return result;
+}
+let b={x:1,y:[1,2,3],z:undefined,p:function test(){}}
+let a = clone(b)
+console.log(a,b,a==b)
+b=[1,2,3];
+console.log(a,b,a==b)
+
+
+// let a={x:1,y:{q:1,w:2},z:undefined};
+// let b = JSON.parse(JSON.stringify(a));
+// b.y.q=4
+// console.log(a,b,a==b)
